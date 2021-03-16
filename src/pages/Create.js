4d3be03c074a5ca901b3f9384f236e4c15a58dc9
Mario Lazzari <mario.lazzari@italcoeng.com>
2021-03-16 12:01:29 +0100

    Done lesson 4

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,14 @@
+import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+//import ButtonGroup from "@material-ui/core/ButtonGroup";
+// icons
+import SendIcon from "@material-ui/icons/Send";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 
 const Create = () => {
   return (
-    <div>
+    <Container>
       <Typography
         variant="h6"
         component="h2"
@@ -12,8 +17,26 @@ const Create = () => {
       >
         Create new note
       </Typography>
-      <Button>ok</Button>
-    </div>
+      <Button
+        variant="contained"
+        type="submit"
+        color="secondary"
+        onClick={() => console.log("click")}
+        //startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRight />}
+      >
+        Submit
+      </Button>
+
+      {/*
+      <ButtonGroup color="secondary">
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+      </ButtonGroup>
+
+      */}
+    </Container>
   );
 };
 
