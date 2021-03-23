f1c302410a1e59b57f58e222da0857b6067bd311
Mario Lazzari <mario.lazzari@gmail.com>
2021-03-23 19:33:37 +0100

    Done lesson 10

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -3,7 +3,7 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 
-function Notes() {
+const Notes = () => {
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
@@ -12,22 +12,10 @@ function Notes() {
       .then(data => setNotes(data));
   }, []);
 
+  console.log("object", notes);
+
   return (
     <Container>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>3</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>4</Paper>
-        </Grid>
-      </Grid>
       <Grid container spacing={3}>
         {notes.map(note => (
           <Grid item xs={12} md={6} lg={4} key={note.id}>
@@ -37,6 +25,6 @@ function Notes() {
       </Grid>
     </Container>
   );
-}
+};
 
 export default Notes;
